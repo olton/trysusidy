90e50c24da590defe7fb2336d68b24e3c3273a21
olton <serhii@pimenov.com.ua>
2024-07-25 16:52:22 +0300

    add logo, hero background

--- metro/index.js
+++ metro/index.js
@@ -1,4 +1,1 @@
-import "@olton/metroui/source/runtime.js"
-import "@olton/metroui/source/common-css/index.js"
-import "@olton/metroui/source/components/grid"
-import "@olton/metroui/source/components/button"
+import "@olton/metroui"
