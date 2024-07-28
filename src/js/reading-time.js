3b1863bf6b5e226f0f869c6aebe62876d1f60c68
olton <serhii@pimenov.com.ua>
2024-07-28 20:19:54 +0300

    ...

--- src/js/reading-time.js
+++ src/js/reading-time.js
@@ -5,6 +5,6 @@ export function readingTime() {
     return function (tree, { data }) {
         const textOnPage = toString(tree);
         const readingTime = getReadingTime(textOnPage);
-        data.astro.frontmatter.time = `Час читання займе ${Math.ceil(readingTime.minutes)} хвил.`;
+        data.astro.frontmatter.time = `Читання займе ${Math.ceil(readingTime.minutes)} хвил.`;
     };
 }
