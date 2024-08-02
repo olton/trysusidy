1fee8f2d9c60107e19683504da3a66f5b2782078
olton <serhii@pimenov.com.ua>
2024-08-02 11:44:43 +0300

    ...

--- src/js/email.js
+++ src/js/email.js
@@ -1,7 +1,6 @@
 const TOKEN = `e520c17d-f28e-4154-bfe9-278c78f54fd4`
 
 globalThis.sendRequestForAdsBlock = async (form) => {
-    console.log(form["name"].value)
     const result = await Email.send({
         SecureToken : TOKEN,
         To : 'serhii@pimenov.com.ua',
@@ -23,6 +22,41 @@ globalThis.sendRequestForAdsBlock = async (form) => {
     })
     if (result.toLowerCase() === "ok") {
         Metro.toast.create("Дякуємо! Об1яву відправлено на розгляд адміністрації сайта.", {
+            clsToast: "success",
+            showTop: true,
+        })
+        setTimeout(() => {
+            window.location.href="/ads"
+        }, 2000)
+    } else {
+        Metro.toast.create(`Помилка! ${result}`, {
+            clsToast: "alert",
+            showTop: true,
+        })
+    }
+}
+
+globalThis.sendAppealForHead = async (form) => {
+    const result = await Email.send({
+        SecureToken : TOKEN,
+        To : 'serhii@pimenov.com.ua',
+        From : "site@trysusidy.kyiv.ua",
+        Subject : "ОСББ Три сусіди 1 - Звернення до голови",
+        Body : `
+    ФІО: ${form["name"].value}<br/>                               
+    Телефон: ${form["tel"].value}<br/>                                
+    Email: ${form["email"].value}<br/>
+    -----------------------<br/>                                
+    Адреса: ${form["house"].value}, кв/оф. ${form["app"].value}<br/>
+    Терміново: ${form["urgently"].checked ? "Так" : "Ні"}<br/>
+    -----------------------<br/>
+    ${form["question"].value}<br/>
+    -----------------------<br/>
+    Лист відправлено з trysusidy.kiev.ua                                
+        `
+    })
+    if (result.toLowerCase() === "ok") {
+        Metro.toast.create("Дякуємо! Ваше звернення відправлено на розгляд.", {
             clsToast: "success",
             showTop: true,
         })
